ab70c04d73b264cd3a3be4e1f44ff133c84f8b19
Pavel Tikhomirov <pavtih@gmail.com>
2010-10-02 22:14:32 +0400

    Measure value control style updated.

--- src/www/js/Application/MeasureValueControl.js
+++ src/www/js/Application/MeasureValueControl.js
@@ -24,11 +24,15 @@ Application.MeasureValueControl = {
      */
     handleMeasurePartial: function (event) {
         var geometry = event.geometry;
+        this.updateText(event);
+    },
+
+    updateText: function(event) {
         var out = "";
         if(event.order == 1) {
-            out += "measure: " + event.measure.toFixed(3) + " " + event.units;
+            out += "Длина: " + event.measure.toFixed(3) + " " + event.units;
         } else {
-            out += "measure: " + event.measure.toFixed(3) + " " + event.units + "<sup>2</" + "sup>";
+            out += "Площадь: " + event.measure.toFixed(3) + " " + event.units + "<sup>2</" + "sup>";
         }
         this.div.update(out);
     },
@@ -40,24 +44,35 @@ Application.MeasureValueControl = {
 
     },
 
+    activate: function() {
+        OpenLayers.Control.prototype.activate.apply(this, arguments);
+        this.updateText({
+            order: 1,
+            measure: 0,
+            units: ""
+        });
+    },
+
     /**
      * @param {OpenLayers.Control.Measure} measureControl
      */
     setMeasureControl: function (measureControl) {
-        // unregister old
-        if (this.measureControl) {
-            this.measureControl.events.unregister("activate", this, this.activate);
-            this.measureControl.events.unregister("deactivate", this, this.deactivate);
-            this.measureControl.events.unregister("measurepartial", this, this.handleMeasurePartial);
-            this.measureControl.events.unregister("measure", this, this.handleMeasureComplete);
-        }
-        this.measureControl = measureControl;
-        // register new
-        if (this.measureControl) {
-            this.measureControl.events.register("activate", this, this.activate);
-            this.measureControl.events.register("deactivate", this, this.deactivate);
-            this.measureControl.events.register("measurepartial", this, this.handleMeasurePartial);
-            this.measureControl.events.register("measure", this, this.handleMeasureComplete);
+        if (this.measureControl != measureControl) {
+            // unregister old
+            if (this.measureControl) {
+                this.measureControl.events.unregister("activate", this, this.activate);
+                this.measureControl.events.unregister("deactivate", this, this.deactivate);
+                this.measureControl.events.unregister("measurepartial", this, this.handleMeasurePartial);
+                this.measureControl.events.unregister("measure", this, this.handleMeasureComplete);
+            }
+            this.measureControl = measureControl;
+            // register new
+            if (this.measureControl) {
+                this.measureControl.events.register("activate", this, this.activate);
+                this.measureControl.events.register("deactivate", this, this.deactivate);
+                this.measureControl.events.register("measurepartial", this, this.handleMeasurePartial);
+                this.measureControl.events.register("measure", this, this.handleMeasureComplete);
+            }
         }
     }
 
